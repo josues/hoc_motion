3c3268a80e77d61f6101cd9ed57720fde8c074fe
josues <josues@mail.de>
2016-12-04 15:42:40 +0100

    Create jp_hoc_motion.js

---
+++ jp_hoc_motion.js
@@ -0,0 +1,19 @@
+var h = 500;
+var k = h/2;
+var n = 100;
+
+function setup(){ 
+  createCanvas(500,500); 
+}
+
+function draw(){
+background(255, 213, 211);
+stroke(255, 255, 255);
+fill(187, 247, 188);
+for(var i = 1; i < 2000; i++){
+  var t = i*0.01;
+  var r = k*cos((n+k*n/h)*t);
+  ellipse(k+sin(t)*r,k+cos(t)*r,5,5);
+}
+n += 0.0005;
+}
